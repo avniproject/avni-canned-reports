d35fab08a6c73d7c7c009c5dee6f09a19334eebe
Vinay Venu <ijkvinay@gmail.com>
2021-11-13 12:32:07 +0530

    Add list of forms on Data

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export default {
     fetchActivities: () => axios.get("/report/aggregate/activities")
-        .then(res => res.data)
+        .then(res => res.data),
+    fetchForms: () => axios.get("/web/forms?size=500", {headers: {'USER-NAME': 'priyankaK@shelter'}})
+      .then(res => res.data._embedded.basicFormDetailses)
 }
--- src/screens/DataReportScreen.js
+++ src/screens/DataReportScreen.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import apis from "../api";
+import Spinner from "../components/Spinner";
+
+const FormItem = (form) => {
+  return (<p>{form.name}</p>)
+};
+
+const FormList = ({forms}) => {
+  return (
+    <div>
+      {forms.map((form) => FormItem(form))}
+    </div>
+  )
+};
+
 
 export default function DataReportScreen() {
-    return (
-        <div>
-            Data report screen
-        </div>
-    )
+  const [forms, setForms] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    apis.fetchForms().then(data => {
+      console.log('data', data);
+      setForms(data);
+      setLoading(false);
+    })
+  }, []);
+
+  return (
+    loading ? <Spinner/> : <FormList forms={forms}/>
+  )
 }
