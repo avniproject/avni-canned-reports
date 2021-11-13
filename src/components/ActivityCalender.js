d9ab5019880f79fc5825a1609ac8a692c13fca14
Vinod Deolal <vinoddeolal@gmail.com>
2021-11-13 13:59:03 +0530

    for the activity calender dynamically calculate the maxValue from the data

--- src/components/ActivityCalender.js
+++ src/components/ActivityCalender.js
@@ -1,14 +1,20 @@
 import {ResponsiveCalendar} from '@nivo/calendar'
 import React from 'react';
+import {maxBy, filter} from 'lodash';
+import moment from "moment";
 
 export default function ActivityCalender({data}) {
+    const currentYear = 2021;
+    const from = `${currentYear}-01-01`;
+    const to = `${currentYear}-12-01`;
+    const maxValue = maxBy(filter(data, ({day}) => moment(day).isBetween(moment(from), moment(to))), ({ value }) => value);
 
     return (
         <div style={{height: 260, flex: 1}}>
             <ResponsiveCalendar
                 data={data}
-                from="2021-01-01"
-                to="2021-12-01"
+                from={from}
+                to={to}
                 emptyColor="#eeeeee"
                 colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
                 margin={{top: 40, right: 40, bottom: 40, left: 40}}
@@ -17,7 +23,7 @@ export default function ActivityCalender({data}) {
                 dayBorderWidth={2}
                 dayBorderColor="#ffffff"
                 minValue={1}
-                maxValue={500}
+                maxValue={maxValue.value}
                 legends={[
                     {
                         anchor: 'bottom-right',
